8e2fd9baca87537c5be61400ed113efefd89291c
RamyaVMurugan <ramyasumothy@gmail.com>
2023-06-01 00:30:23 -0400

    Added code to check invalid user login
    Inserted code to check the displayed error message but unable to crack how to compare the values of expected and displayed. Need assistance here.

--- test/specs/2_LOGIN_invalid_user.spec.js
+++ test/specs/2_LOGIN_invalid_user.spec.js
@@ -1,4 +1,5 @@
-const LoginScreen = require("../screenobjects/login.screen");
+const loginScreen = require("../screenobjects/login.screen");
+//const LoginScreen = require("../screenobjects/login.screen");
 
 describe('2_LOGIN_invalid_user', () => {
 
@@ -10,6 +11,26 @@ describe('2_LOGIN_invalid_user', () => {
     // 2. Insert 'invalid_password' to the Password field
     // 3. Press Login
     // 4. Error message is shown: "Username and password do not match any user in this service"
+    await loginScreen.usernameField.setValue("invalid_username");
+    await loginScreen.passwordField.setValue("invalid_password");
+    await loginScreen.loginButton.click();
+
+    await expect(loginScreen.errorMsgField).toBeDisplayed();
+    await expect(loginScreen.errorMsgField).toHaveText('Username and password do not match any user in this service');
+    
+/*    if(expect(loginScreen.errorMsgField).toHaveText('Username and password do not match any user in this service')==true)
+      console.log("Invalid credentials error message is displayed successfully")
+    else  
+      console.log("Failed to display expected error message");
+*/
+      
+ /*   let errorMsg = loginScreen.errorMsgField.getValue();
+    if(errorMsg = "Username and password do not match any user in this service")
+      console.log("Invalid credentials error message is displayed successfully")
+    else  
+      console.log("Failed to display expected error message");
+      */
+      
   });
   
 });
