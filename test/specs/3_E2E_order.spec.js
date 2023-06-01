03327e5215a3594a8c806c97b489e721efc2f731
kollonqvist-rbi <ollonqvistkalle@gmail.com>
2023-06-01 08:19:32 -0700

    add 3rd test case

---
+++ test/specs/3_E2E_order.spec.js
@@ -0,0 +1,32 @@
+const LoginScreen = require("../screenobjects/login.screen");
+
+describe('3_E2E_order', () => {
+
+  it('3_E2E_order', async () => {
+    // CREATE A TEST CASE CONTAINING THE FOLLOWING STEPS:
+    // 
+    // 1. Login to the app with valid credentials
+    // 2. Scroll to the Test.allTheThings() T-Shirt (Red)
+    // 3. Open the T-Shirt item page
+    // 4. Add the T-Shirt to the cart
+    // 5. Open Cart
+    // 6. Verify that the item is in the Cart
+    // 7. Perform a swipe from left to right to navigate back to the product page
+    // 8. Verify that product page was opened after the swipe
+    // 9. Open Cart
+    // 10. Click Checkout
+    // 11. Insert first name, last name and postal code
+    // 12. Click Continue
+    // 13. Click Finish
+    // 14. Verify that the the THANK YOU FOR YOU ORDER is displayed
+
+    // CLEANUP:
+
+    // Click back to home
+    // Open the left side panel
+    // Click log out
+    // Verify that the user was signed out
+
+  });
+  
+});
