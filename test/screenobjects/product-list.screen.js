7a484a4e37b18f723d9f7bb0fdd75a50f1c5fc08
Aurelian Voicu <aurelian.voicu@accenture.com>
2023-06-05 12:54:27 -0400

    Completed the first 5 steps of the exercise.

--- test/screenobjects/product-list.screen.js
+++ test/screenobjects/product-list.screen.js
@@ -5,6 +5,11 @@ class ProductListScreen {
   get menuButton() {
     return $('//*[@content-desc="test-Menu" or @name="test-Menu"]');
   }
+  get productItem() {
+    return $('(//android.widget.TextView[@content-desc="test-Item title"])[2]');
+    //return $('(//*[@content-desc="test-Item title"])[2]');
+
+  }
 }
 
 module.exports = new ProductListScreen();
