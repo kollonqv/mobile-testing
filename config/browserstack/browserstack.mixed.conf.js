85ad11f055703e6cea3f97a51c23584201758b3f
Yash <104842864+mittalyash01@users.noreply.github.com>
2023-06-15 14:47:53 -0400

    config

--- config/browserstack/browserstack.mixed.conf.js
+++ config/browserstack/browserstack.mixed.conf.js
@@ -31,10 +31,10 @@ config.capabilities = [
   {
     user: process.env.BROWSERSTACK_USERNAME || user,
     key: process.env.BROWSERSTACK_ACCESS_KEY || key,
-    "platformName" : "android",
-    "appium:platformVersion" : process.env.npm_config_osversion || "12.0",
-    "appium:deviceName" : process.env.npm_config_device || "Samsung Galaxy S22 Ultra",
-    "appium:app" : "bs://9d6072c53c0af842cee2d72c0c22284e75af7cc9",
+    "platformName" : "ios",
+    "appium:platformVersion" : process.env.npm_config_osversion || "16.0",
+    "appium:deviceName" : process.env.npm_config_device || "iPhone 14",
+    "appium:app" : "bs://59821aec5c61ea1f33282a9bf2ee2c5770ee51d3",
   }
 ]
 
