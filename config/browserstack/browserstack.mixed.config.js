bc1c5a351e42565bf72766143df7054c9fb9d7e3
Yash <104842864+mittalyash01@users.noreply.github.com>
2023-06-15 12:53:32 -0400

    browserstack integration

--- config/browserstack/browserstack.mixed.config.js
+++ config/browserstack/browserstack.mixed.config.js
@@ -18,6 +18,16 @@ config.specs = [
 // Capabilities
 // ============
 config.capabilities = [
+  //iOS config
+  {
+    user: process.env.BROWSERSTACK_USERNAME || user,
+    key: process.env.BROWSERSTACK_ACCESS_KEY || key,
+    "platformName" : "android",
+    "appium:platformVersion" : process.env.npm_config_osversion || "12.0",
+    "appium:deviceName" : process.env.npm_config_device || "Samsung Galaxy S22 Ultra",
+    "appium:app" : "bs://9d6072c53c0af842cee2d72c0c22284e75af7cc9",
+  },
+  //android config
   {
     user: process.env.BROWSERSTACK_USERNAME || user,
     key: process.env.BROWSERSTACK_ACCESS_KEY || key,
