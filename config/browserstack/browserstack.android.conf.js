4559010c45a771c3513ff7d4a2dcf2f9b7b7598f
Yash <104842864+mittalyash01@users.noreply.github.com>
2023-06-16 15:32:29 -0400

    android live video

--- config/browserstack/browserstack.android.conf.js
+++ config/browserstack/browserstack.android.conf.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { config } = require('../wdio.shared.conf');
-const user= '';
-const key = '';
+const user= 'yashshvimittal_dODRYg';
+const key = '4mxThL7TcWipJwJTfb3y';
 
 
 config.hostname = ('hub.browserstack.com');
@@ -25,6 +25,9 @@ config.capabilities = [
     "appium:platformVersion" : process.env.npm_config_osversion || "12.0",
     "appium:deviceName" : process.env.npm_config_device || "Samsung Galaxy S22 Ultra",
     "appium:app" : "bs://9d6072c53c0af842cee2d72c0c22284e75af7cc9",
+    "appium:interactiveDebugging" : true,
+    "appium:video" : true
+
   }
 ]
 
